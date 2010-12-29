5cc76fbc8c1e6fe55e6894b5d7c784b5cda3bf3d
Olivier van der Aa <o.vanderaa@gmail.com>
2010-12-29 17:47:45 +0000

    web frontend for measurement database

---
+++ bew.py
@@ -0,0 +1,50 @@
+import gviz_api
+import pymongo 
+import sys
+from twisted.internet import reactor
+from twisted.web.server import Site
+from twisted.web.resource import Resource
+from twisted.web.static import File
+import datetime 
+import time
+
+class BeQuery(Resource):
+    isLeaf = True
+    def __init__(self, bees):
+        Resource.__init__(self)
+        self.bees=bees
+    def render_GET(self, request):
+        print request.args
+        start=request.args['start'][0]
+        stop=request.args['stop'][0]
+        tqx=request.args['tqx'][0]
+        reqID=tqx.split(':')[1]
+#        print start
+#        stop=self.get_argument('stop')
+        print start, stop, reqID
+        data=[]
+        st=datetime.datetime.strptime(start,'%d-%m-%Y')
+        et=datetime.datetime.strptime(stop,'%d-%m-%Y')
+        sepoch = time.mktime(st.timetuple())
+        eepoch = time.mktime(et.timetuple())
+ 	description = {"time": ("datetime", "time"),
+        	       "value": ("number", "value")} 
+        query={"timestamp": {"$gt": sepoch, "$lt": eepoch}, 'detector':'Ruche 6'}
+        opt={'timestamp':1,'data':1}
+        for m in self.bees.find(query,opt):
+	    epoch = datetime.datetime.fromtimestamp(m['timestamp'])
+	    data.append({'time':epoch,'value':m['data']})
+        data_table = gviz_api.DataTable(description)
+        data_table.LoadData(data)
+        request.setHeader("Content-Type", "text/plain; charset=utf-8")
+        return data_table.ToJSonResponse(columns_order=("time", "value"), order_by="time", req_id=reqID)
+
+
+server = pymongo.Connection()
+db  = server.daq
+root = Resource()
+root.putChild( "static", File("/home/vanderaa/becouch/static") )
+root.putChild( "query", BeQuery(db.bees) )
+factory = Site(root)
+reactor.listenTCP(8880, factory)
+reactor.run()
