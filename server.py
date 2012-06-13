bbe8496faa41246d3a4c5275cf88a19302b66beb
Olivier van der Aa <o.vanderaa@gmail.com>
2012-06-13 12:33:18 +0200

    added the plotting and server stuff

---
+++ server.py
@@ -0,0 +1,29 @@
+import tornado.ioloop
+import tornado.web
+from urlparse import parse_qs as qs_decode
+from urllib import urlencode as qs_encode
+from tornado import escape
+from plot import plotter
+from tsdb import TSDb
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.set_status(200)
+        self.set_header("Content-Type", "image/svg+xml")
+        db = TSDb('lego.xe.be',4242)
+        data = db.query('2011/06/09-12:00:00',None,['ohain.temp','ohain.adc{id=14}'],'sum','1h-avg')
+        query = qs_decode(self.request.query)
+
+
+
+        self.finish(plotter(data))
+
+if __name__ == "__main__":
+    application = tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+    application.listen(8081)
+
+    print("localhost:8080")
+
+    tornado.ioloop.IOLoop.instance().start()
---
+++ tsdb.py
@@ -0,0 +1,52 @@
+import urllib 
+
+
+class TSDb:
+    def __init__(self, host,port):
+        self.host = host
+        self.port = port
+    def query(self, start, stop, metrics, aggr, filt=None):
+        # Lets construct the query strucutre
+        qry = [('ascii',''),('start',start)]
+        if( stop ):
+            qry.append(('end',stop))
+        for m in metrics:
+            if( filt ):
+                qry.append( ('m',aggr+':'+filt+':'+m) )
+            else:
+                qry.append( ('m',aggr+':'+m) )
+        print qry
+        url = urllib.urlencode(qry)
+        # Now lets query the guy
+        f = urllib.urlopen('http://'+self.host+':'+str(self.port)+'/q?%s' % url)
+        # Check the status code 
+#        print f.geturl()
+        if( f.getcode() == 200 ):
+            return self.parse_result(f)
+        else:
+            f.close()
+            return None
+
+    def parse_result(self, f):
+        res = {}
+        try:
+            for line in f:
+                atoms=line.split()
+                ln = len(atoms)
+                if( ln >= 3 ):
+                    tstamp = int(atoms[1])
+                    value  = float(atoms[2])
+                    if( ln == 3 ):
+                        key = atoms[0]
+                    if( ln == 4 ):
+                        key = atoms[0]+'{'+atoms[3]+'}'
+                    try:
+                        res[key][0].append(tstamp)
+                        res[key][1].append(value)
+                    except KeyError:
+                        res[key]=([],[])
+                        res[key][0].append(tstamp)
+                        res[key][1].append(value)
+        finally:
+            f.close()
+        return res
