63adc2395a4528e27dcfb48eb35dc12633fb33f0
Olivier van der Aa <o.vanderaa@gmail.com>
2012-06-15 16:58:53 +0200

    simple code to publish udp messages

---
+++ udpcol.py
@@ -0,0 +1,31 @@
+import socket
+import json
+import telnetlib
+import time 
+
+socks = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+socks.bind(('',9876))
+
+queue = []
+
+def publish(host, port, z ):
+    t = telnetlib.Telnet(host, port)
+    for q in z:
+        ts = q[0]
+        d  = q[1]
+        ss = 'put cam.temp '+str(ts)+' '+str(d['TempCambridge'])+' id=0\n'
+        t.write(ss);
+        print ss, 
+    t.close()
+    z[:] = []
+
+
+while True:
+    data,addr = socks.recvfrom(1024)
+    data = json.loads(data)
+    ts = int(time.time())
+    queue.append((ts,data))
+    print data,addr
+    if(len(queue) > 1 ):
+        print 'publishing'
+        publish('localhost',4242,queue)
