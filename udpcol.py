3ca3308bb4458cba97cc509fa6ef182bab344213
Olivier van der Aa <o.vanderaa@gmail.com>
2012-06-19 12:35:10 +0200

    working version

--- udpcol.py
+++ udpcol.py
@@ -12,22 +12,35 @@ socks.bind(('',3001))
 
 queue = []
 
-def publish(host, port, z ):
+def publish(host, port, ts, data ):
     t = telnetlib.Telnet(host, port)
-    for q in z:
-        ts = q[0]
-        d  = q[1]
-        ss = 'put cam.temp '+str(ts)+' '+str(d['TempCambridge'])+' id=0\n'
-        t.write(ss);
-#        print ss, 
+    query =''
+    for k in data.keys():
+        i = 0
+        for v in data[k]:
+            query = query   + 'put ' + \
+                    str(k)  + ' '    + \
+                    str(ts) + ' '    + \
+                    str(v)  + ' '    + \
+                    'id='   + str(i) + '\n'
+            i=i+1
+    t.write(query)
     t.close()
-    z[:] = []
+    #print query
+#    for q in z:
+#        ts = q[0]
+#        d  = q[1]
+#        ss = 'put cam.temp '+str(ts)+' '+str(d['TempCambridge'])+' id=0\n'
+#        t.write(ss);
+#        print ss, 
+#    t.close()
+#    z[:] = []
 
 
 def calibrate( fname, data ):
     f = open(fname,'r');
     cal = yaml.load(f)
-    print cal
+#    print cal
     f.close()
     # For each element in data se
     # if we have a element to calibrate
@@ -48,12 +61,11 @@ while True:
     data,addr = socks.recvfrom(1024)
     data = json.loads(data)
     # We need to calibrate the data
-    print data
     ts = int(time.time())
     calibrate('cal.yaml',data)
-    print data
+    publish('localhost',4242, ts, data)
     queue.append((ts,data))
-    print '.', 
-    if(len(queue) > 20 ):
-        print 'P'
+#    print '.', 
+#    if(len(queue) > 20 ):
+#        print 'P'
 #        publish('localhost',4242,queue)
