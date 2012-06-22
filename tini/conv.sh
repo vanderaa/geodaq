160e300a1c22d42dbcdb02bd08130691926b9183
Olivier van der Aa <o.vanderaa@gmail.com>
2012-06-22 11:34:09 +0200

    more sensble name

--- tini/conv.sh
+++ tini/conv.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 HH=/home/vanderaa/SW/TINI/tini1.02b
-java -classpath $HH/bin/tini.jar BuildDependency -f build -o bin/test.tini -d $HH/bin/tini.db -p $HH/bin/owapi_dependencies_TINI_001.jar -add OneWireContainer10
+java -classpath $HH/bin/tini.jar BuildDependency -f build -o bin/tinibeeo.tini -d $HH/bin/tini.db -p $HH/bin/owapi_dependencies_TINI_001.jar -add OneWireContainer10
