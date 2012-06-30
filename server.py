0f3862ffe4c747d2813b67c529aaa6a7c641fc1a
Olivier van der Aa <o.vanderaa@gmail.com>
2012-06-30 18:49:08 +0200

    server with arg

--- server.py
+++ server.py
@@ -15,17 +15,17 @@ class MainHandler(tornado.web.RequestHandler):
         det = ['ohain.temp','ohain.weight{id=1}','ohain.weight{id=2}','ohain.weight{id=0}']
         query = qs_decode(self.request.query)
         print query['start']
-        data = db.query(query['start'][0],None,det,'sum','10m-avg')
+        data = db.query(query['start'][0],None,det,'sum','1h-avg')
 #        print data
-
-
         self.finish(plotter(data))
+        del data
+        del db
 
 if __name__ == "__main__":
     application = tornado.web.Application([
         (r"/", MainHandler),
     ])
-    application.listen(8080,'0.0.0.0')
+    application.listen(8080,'127.0.0.1')
 
     print("localhost:8080")
 
