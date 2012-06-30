660ea68ef8e06bc278387acbfc3b6217145c7971
Olivier van der Aa <o.vanderaa@gmail.com>
2012-06-30 18:48:38 +0200

    make sure we generate directory

--- push.sh
+++ push.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 whome=$HOME/geodaq/web
-wget -O $whome/img/plot_1w.svg http://127.0.0.1:8080/?start=1w-ago
-wget -O $whome/img/plot_1m.svg http://127.0.0.1:8080/?start=30d-ago
-wget -O $whome/img/plot_1d.svg http://127.0.0.1:8080/?start=1d-ago
-wget -O $whome/img/plot_1y.svg http://127.0.0.1:8080/?start=1y-ago
+$HOME/geodaq/genplot.py 1w-ago $whome/img/plot_1w.svg
+$HOME/geodaq/genplot.py 30d-ago $whome/img/plot_1m.svg
+$HOME/geodaq/genplot.py 1d-ago $whome/img/plot_1d.svg
+$HOME/geodaq/genplot.py 1y-ago $whome/img/plot_1y.svg
+#wget -O $whome/img/plot_1w.svg http://127.0.0.1:8080/?start=1w-ago
+#wget -O $whome/img/plot_1m.svg http://127.0.0.1:8080/?start=30d-ago
+#wget -O $whome/img/plot_1d.svg http://127.0.0.1:8080/?start=1d-ago
+#wget -O $whome/img/plot_1y.svg http://127.0.0.1:8080/?start=1y-ago
 s3cmd  put -P --recursive $whome/* s3://ruches.xe.be/
