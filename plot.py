9ca7d80ba8354546bc142cb3e8400c418fd003d1
Olivier van der Aa <o.vanderaa@gmail.com>
2012-07-07 22:52:46 +0200

    don't know what I changed

--- plot.py
+++ plot.py
@@ -6,6 +6,7 @@ matplotlib.use('Agg') # So we can run this headless
 import matplotlib.pyplot as plt
 from matplotlib import dates
 from matplotlib import rc
+import pytz
 #from matplotlib import gridspec 
 
 #rc('text',usetex=True)
@@ -13,15 +14,17 @@ from matplotlib import rc
 
 def plotter( data, fname=None ):
 
+    btz = pytz.timezone('UTC')
     xy = data['ohain.temp{id=0}']
 
     fig = plt.figure()
  #   gs = gridspec.GridSpec(1, 2,width_ratios=[2,1])
     #p1 = fig.add_subplot(2,1,1)
     p1 = plt.subplot2grid((3,1),(0,0))
+    p1.set_title('Time is UTC')
     #p2 = fig.add_subplot(2,1,2) 
     p2 = plt.subplot2grid((3,1),(1,0),rowspan=2)
-    p1.plot_date(dates.epoch2num(xy[0]),xy[1],'r-',tz=None,xdate=True,ydate=False,aa=False,lw=0.5)
+    p1.plot_date(dates.epoch2num(xy[0]),xy[1],'r-',tz=btz,xdate=True,ydate=False,aa=False,lw=0.5)
     p1.set_ylim(0,35)
     majloc = dates.AutoDateLocator()
     majform = dates.AutoDateFormatter(majloc)
@@ -37,11 +40,11 @@ def plotter( data, fname=None ):
     
   #  p1 = fig.add_subplot(212) 
     xy1 = data['ohain.weight{id=1}']
-    p2.plot_date(dates.epoch2num(xy1[0]),xy1[1],'g-',tz=None,xdate=True,ydate=False,aa=True,lw=0.5)
+    p2.plot_date(dates.epoch2num(xy1[0]),xy1[1],'g-',tz=btz,xdate=True,ydate=False,aa=True,lw=0.5)
     xy2 = data['ohain.weight{id=2}']
-    p2.plot_date(dates.epoch2num(xy2[0]),xy2[1],'b-',tz=None,xdate=True,ydate=False,aa=True,lw=0.5)
+    p2.plot_date(dates.epoch2num(xy2[0]),xy2[1],'b-',tz=btz,xdate=True,ydate=False,aa=True,lw=0.5)
     xy3 = data['ohain.weight{id=0}']
-    p2.plot_date(dates.epoch2num(xy3[0]),xy3[1],'m-',tz=None,xdate=True,ydate=False,aa=True,lw=0.5)
+    p2.plot_date(dates.epoch2num(xy3[0]),xy3[1],'m-',tz=btz,xdate=True,ydate=False,aa=True,lw=0.5)
     p2.set_ylabel('Weight (kg)')
     p2.grid(True)
     leg=p2.legend(['1','2','3'],'upper left', labelspacing=0.2)
